492e34a1130fdb2dade5aa1e54101a5144a79861
Antonin <antonin.chenavaz@gmail.com>
2026-02-20 22:13:46 +0100

    Delete bot directory

--- bot/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Rend le dossier `bot/` importable comme package Python.
-
-Sans ce fichier, selon le contexte d'exécution, `python -m bot` et
-les imports de type `bot.admin` peuvent devenir fragiles.
-"""
